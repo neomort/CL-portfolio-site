4b36efebf1f6776e6d5c7385f98093892ef44d6a
Kyle Adams <kyle@kyleadams.com>
2021-12-08 16:30:19 +0400

    document & favicons added

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  
+
+  render() {
+    return (
+      <Html>
+        <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons//favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons//favicon-16x16.png" />
+        <link rel="manifest" href="/images/favicons//site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
